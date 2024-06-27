03071d6cd276d884bb1e9d4cf382299d0fcb8775
Allen Park <allenpark@Allens-MacBook-Pro-2.local>
2024-06-27 18:20:57 -0400

    added id to clarity script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script
+        id="microsoft-clarity"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
