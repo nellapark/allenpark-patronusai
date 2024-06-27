8a83088d12824261962c489ef2450f1eb8c1c24c
Allen Park <allenpark@Allens-MacBook-Pro-2.local>
2024-06-27 17:17:54 -0400

    dont show marquee and calculator on mobile and change card to column flex instead of row on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -84,13 +84,13 @@ export default function Home() {
         </div>
       </div>
       <WaveAnimation />
-      <Marquee />
+      {!isMobile && <Marquee />}
       <div className="flex flex-col items-center w-full py-16 relative overflow-hidden">
         <div className="background-image" />
         <div className="styled-divider"></div>
         <div className="z-10 w-5/6 flex flex-col items-center gap-16 py-24" ref={aboutRef}>
           <h1 className={`${isMobile ? "text-header-mobile" : "text-header"}`}>About Me</h1>
-          <div className="flex justify-between w-full">
+          <div className={`flex justify-between w-full ${isMobile ? "flex-col" : "flex-row"}`}>
             <Card cardNumber={0} />
             <Card cardNumber={1} />
             <Card cardNumber={2} />
@@ -101,19 +101,21 @@ export default function Home() {
           <Gallery />
         </div>
       </div>
-      <div className="flex flex-col items-center w-full py-32 gradient-background" ref={whyPatronusRef}>
-        <div className="flex items-center w-5/6 text-wrap justify-between">
-          <div className="w-1/2 h-full pr-20 gap-4 flex flex-col break-words font-light">
-            <h1 className="normal-text-header-dark">LLM Mistakes <br />are Costly</h1>
-            <div className="text-[var(--solid-colors--black)] text-lg">
-              Hallucinations have caused <a href="https://www.washingtonpost.com/technology/2023/11/16/chatgpt-lawyer-fired-ai/" target="_blank"><u>fines</u></a> and <a href="https://web.archive.org/web/20240610032818/https://asim.bearblog.dev/how-a-single-chatgpt-mistake-cost-us-10000/" target="_blank"><u>expensive coding mistakes</u></a>.
-              The liability of LLMs has led to ~1/4 of companies <a href="https://techmonitor.ai/technology/ai-and-automation/firms-generative-ai-ban" target="_blank"><u>banning</u></a> the use of generative AI.
-              <a href="https://www.deepkeep.ai/llamav2-7b-analysis" target="_blank"><u>Llama 2 7B</u></a>, <a href="https://pubmed.ncbi.nlm.nih.gov/38776130/" target="_blank"><u>GPT-4</u></a>, <a href="https://www.researchgate.net/publication/380887560_Reducing_LLM_Hallucination_Using_Knowledge_Distillation_A_Case_Study_with_Mistral_Large_and_MMLU_Benchmark" target="_blank"><u>Mistral Large</u></a>, and <a href="https://arxiv.org/html/2401.03205v1" target="_blank"><u>Falcon 40B</u></a> are all known to hallucinate.
+      {!isMobile && (
+        <div className="flex flex-col items-center w-full py-32 gradient-background" ref={whyPatronusRef}>
+          <div className="flex items-center w-5/6 text-wrap justify-between">
+            <div className="w-1/2 h-full pr-20 gap-4 flex flex-col break-words font-light">
+              <h1 className="normal-text-header-dark">LLM Mistakes <br />are Costly</h1>
+              <div className="text-[var(--solid-colors--black)] text-lg">
+                Hallucinations have caused <a href="https://www.washingtonpost.com/technology/2023/11/16/chatgpt-lawyer-fired-ai/" target="_blank"><u>fines</u></a> and <a href="https://web.archive.org/web/20240610032818/https://asim.bearblog.dev/how-a-single-chatgpt-mistake-cost-us-10000/" target="_blank"><u>expensive coding mistakes</u></a>.
+                The liability of LLMs has led to ~1/4 of companies <a href="https://techmonitor.ai/technology/ai-and-automation/firms-generative-ai-ban" target="_blank"><u>banning</u></a> the use of generative AI.
+                <a href="https://www.deepkeep.ai/llamav2-7b-analysis" target="_blank"><u>Llama 2 7B</u></a>, <a href="https://pubmed.ncbi.nlm.nih.gov/38776130/" target="_blank"><u>GPT-4</u></a>, <a href="https://www.researchgate.net/publication/380887560_Reducing_LLM_Hallucination_Using_Knowledge_Distillation_A_Case_Study_with_Mistral_Large_and_MMLU_Benchmark" target="_blank"><u>Mistral Large</u></a>, and <a href="https://arxiv.org/html/2401.03205v1" target="_blank"><u>Falcon 40B</u></a> are all known to hallucinate.
+              </div>
             </div>
+            <Calculator />
           </div>
-          <Calculator />
         </div>
-      </div>
+      )}
       <div className="flex flex-col items-center w-full py-32 relative">
         <div className="hidden">Credit to Scale.com for the beautiful website and inspiration for this section!</div>
         <div className="w-5/6 flex flex-col items-center gap-12">
